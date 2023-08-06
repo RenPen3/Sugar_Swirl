9c0ca6ce0461e3ab9a3c840265029590edc59b56
Rene Perez <wrenwiththepen@gmail.com>
2023-08-06 14:06:54 -0700

    I figured out how to switch ham and x for the sub-menu

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const bars = document.querySelector('.fa-bars');
 const x = document.querySelector('.fa-x');
 const menu = document.querySelector('.mobile-container');
+const btns = document.querySelectorAll('.btns');
 
 
-bars.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  bars.style.display = 'none';
-  x.style.display = 'flex';
+function toggleBtn() {
+    btns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        menu.classList.toggle('show');
+        switch (btn) {
+          case bars:
+            removeBars();
+            break;
+          case x:
+            removeX();
+            break;
+          default:
+            console.log('end');
+        }
+
+      })
+    })
+    removeX();
+
+  }
+
+  function removeBars() {
+    bars.style.display = 'none';
+    x.style.display = 'flex';
+  }
+
   
-});
+  function removeX() {
+    x.style.display = 'none';
+    bars.style.display = 'flex';
+  }
+  
+toggleBtn();
 
-x.addEventListener('click', () => {
-  menu.classList.toggle('show');
-  x.style.display = 'none';
-  bars.style.display = 'flex';
-});
+
 
 
 
